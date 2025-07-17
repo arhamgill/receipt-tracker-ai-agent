eda9e79c0f6c4850158e50b599dd1c8446c0328d
Arham Gill <arhamgillcoder@gmail.com>
2025-07-17 13:58:02 +0500

    header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>page 1</div>;
+  return <div></div>;
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,31 @@
+import { SignedOut, SignedIn, UserButton, SignInButton } from "@clerk/nextjs";
+import { Shield } from "lucide-react";
+import Link from "next/link";
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 function Header() {
   return (
-    <div>
-      <Button>Hello</Button>
+    <div className="flex items-center justify-between p-4">
+      <Link href={"/"} className="flex items-center gap-2">
+        <Shield className="h-6 w-6 text-blue-500" />
+        <h1 className="text-xl font-semibold">Receipto</h1>
+      </Link>
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href={"/receipts"}>
+            <Button variant={"outline"}>My Receipts</Button>
+          </Link>
+          <Link href={"/manage-plan"}>
+            <Button>Manage Plan</Button>
+          </Link>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button variant={"default"}>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </div>
   );
 }
